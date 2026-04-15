001616f7d67b9ea86e1c068029a5fc8cb997fe47
Ekbal Hassan <ekbalhassan147@gmail.com>
2026-04-15 12:28:57 +0530

    biggest number among 3

---
+++ conditional_statement/if_else.py
@@ -0,0 +1,43 @@
+# check if a number entered by the user is odd or even
+# user_input = int(input("Enter a number: "))
+
+# if user_input % 2 == 0:
+#     print(f"{user_input} is an even number.")
+# else:
+#     print(f"{user_input} is an odd number.")
+
+
+
+# the greatest of 3 numbers entered by the user
+input1 = int(input("Enter the first number: "))
+input2 = int(input("Enter the second number: "))
+input3 = int(input("Enter the third number: "))
+
+# using built-in max function
+
+# max_num = max(input1, input2, input3)
+# print(f"The greatest number among {input1}, {input2}, and {input3} is {max_num}.")
+
+# using if-else statement
+# if input1 > input2 and input1 > input3:
+#     print(f"The greatest number among {input1}, {input2}, and {input3} is {input1}.")
+# elif input2 > input1 and input2 > input3:
+#     print(f"The greatest number among {input1}, {input2}, and {input3} is {input2}.")
+# else:
+#     print(f"The greatest number among {input1}, {input2}, and {input3} is {input3}.")
+    
+    
+    
+
+# check if a number is a divisible of 7 or not
+
+# import math
+
+
+# number = int(input("Enter a number: "))
+
+# if number % 7 == 0:
+#     num_divided = math.floor(number/7)
+#     print(f"{number} divided by 7 is {num_divided}.")
+# else:
+#     print(f"{number} is not a divisible of 7.")
