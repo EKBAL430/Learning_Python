e323b767591e84ebef9ff737503479828b3ad3b5
Ekbal Hassan <ekbalhassan147@gmail.com>
2026-04-15 10:35:05 +0530

    greater than or equal to from user input values

---
+++ input/true_or_false.py
@@ -0,0 +1,7 @@
+first_number = int(input("Enter first value: "))
+second_number = int(input("Enter second value: "))
+
+if first_number >= second_number:
+    print("True")
+else:
+    print("False")
