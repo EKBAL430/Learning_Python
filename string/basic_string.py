16d5108279239fcdda5adef26dd11e912aa0ed71
Ekbal Hassan <ekbalhassan147@gmail.com>
2026-04-15 10:58:22 +0530

    string

---
+++ string/basic_string.py
@@ -0,0 +1,19 @@
+# first_name = input("Enter your first name: ")
+# print("Hello, " + first_name + "!")
+
+# print(type(first_name))
+# print("The length of your name is: " + str(len(first_name)))
+
+
+
+
+
+# num = int(input("Enter your first name: "))
+# print("This is the value you entered: " + str(num) + "!")
+
+# print(type(num))
+
+# print("The length of your name is: " + str(len(str(num))))
+
+
+
