d6057847cb76247d7e78a311b31b12ed6f8cf01a
Ekbal Hassan <ekbalhassan147@gmail.com>
2026-04-15 11:49:26 +0530

    string operatioons

---
+++ string/some_module_string.py
@@ -0,0 +1,44 @@
+colleg_name = "bholanath College"
+
+print(str(len(colleg_name)))
+
+print(range(len(colleg_name)))
+
+print(list(range(len(colleg_name))))
+
+print(list(enumerate(colleg_name)))
+
+# print(colleg_name[9])
+# print(colleg_name[5])
+# print(colleg_name[-1])
+
+
+# slicing
+# print(colleg_name[0:9])
+# print(colleg_name[0:10:3])
+# print(colleg_name[-4:-2])
+
+# split
+# print(colleg_name.split(" "))
+
+# join
+# print("_".join(colleg_name))
+
+# remove suffix
+# print(colleg_name.removesuffix("nath College"))
+
+# end with
+# print(colleg_name.endswith("College"))
+
+# capatalize
+# print(colleg_name.capitalize())
+
+# replace
+# print(colleg_name.replace("bholanath", "B.N."))
+
+# find
+# print(colleg_name.find("n"))
+
+
+# count
+# print(colleg_name.count("l")) 
