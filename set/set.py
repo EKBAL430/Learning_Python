ed28dd2ad5e84e0128c8c0356795dac8804ee695
Ekbal Hassan <ekbalhassan147@gmail.com>
2026-04-16 22:34:08 +0530

    set operations and methods.

---
+++ set/set.py
@@ -0,0 +1,42 @@
+num1 = {1, 2, 2, 3, 3, 4, 4, 5}
+
+num2 = {4, 5, 6, 7, 8, 9, 10}
+
+# print(len(num1))
+# print(num1)
+# print(set(num1))
+# print(type(num1))
+
+# union
+num3 = num1.union(num2)
+print(num3)
+
+# intersection
+num4 = num1.intersection(num2)
+print(num4)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+name = {"sham", "khan", "jonny"}
+# print(name)
+# print(type(name))
+
+
+# methods
+# add
+name.add("deep")
+# print(name)
+
+# remove
+name.remove("sham")
+# print(name)
+
+
+# pop
+name.pop()
+# print(name)
+
+# clear
+name.clear()
+# print(name)
---
+++ set/set_notes.md
@@ -0,0 +1,3 @@
+## Notes
+1. Collection of the unordered items.
+2. Each element in the set must be unique & immutable.
